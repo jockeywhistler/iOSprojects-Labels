1a34585e1d9d0fd73a5fc959f0f0558d68bddd11
Allan Wilson <Jockey@allans-macbook-air.local>
2015-10-03 21:46:17 -0700

    Initial Commit

---
+++ Labels/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Labels
+//
+//  Created by Allan Wilson on 2015-10-03.
+//  Copyright © 2015 sigma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
